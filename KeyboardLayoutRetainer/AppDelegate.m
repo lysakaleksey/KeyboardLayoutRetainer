609885d53e6c66f1ba13753d383cfbb0bd5ca6b3
Oleksiy Lysak <olysak@tango.me>
2021-12-22 20:38:23 +0200

    Cleanup

--- KeyboardLayoutRetainer/AppDelegate.m
+++ KeyboardLayoutRetainer/AppDelegate.m
@@ -11,208 +11,109 @@
 
 @implementation AppDelegate
 
--(id)init
-{
+- (id)init {
     self = [super init];
-    if(self)
-    {
+    if (self) {
         _sourceManager = [[SourceManager alloc] init];
     }
-    
+
     return self;
 }
 
--(void)awakeFromNib {
-    
+- (void)awakeFromNib {
+
     // Create the status bar item
     _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:25.0];
     [_statusItem setMenu:menu];
     [_statusItem setHighlightMode:YES];
-    
+
     // load the currect language image
     NSImage *image = [NSImage imageNamed:@"StatusIcon"];
     [image setSize:NSMakeSize(20, 20)];
     [_statusItem setImage:image];
-    
+
     // Init the dictionary
     _retainedLayouts = [NSMutableDictionary dictionary];
-    
-    #if DEBUG
+
+#if DEBUG
     NSLog(@"awakeFromNib");
-    #endif
+#endif
 }
 
--(void)applicationDidFinishLaunching:(NSNotification *)notification
-{
-    #if DEBUG
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+#if DEBUG
     NSLog(@"applicationDidFinishLaunching");
-    #endif
-    
+#endif
     [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
                                                            selector:@selector(applicationActivated:)
                                                                name:NSWorkspaceDidActivateApplicationNotification
                                                              object:nil];
 }
 
--(void)applicationWillTerminate:(NSNotification *)notification
-{
-    #if DEBUG
+- (void)applicationWillTerminate:(NSNotification *)notification {
+#if DEBUG
     NSLog(@"applicationWillTerminate");
-    #endif
-    
+#endif
+
     [[[NSWorkspace sharedWorkspace] notificationCenter] removeObserver:self];
 }
 
--(void)applicationActivated:(NSNotification*)notification
-{
-    #if DEBUG
+- (void)applicationActivated:(NSNotification *)notification {
+#if DEBUG
     NSLog(@"============= Application Activated =============");
-    #endif
-    
-    // Take a running application metadata
-    NSRunningApplication *currentApplication = (NSRunningApplication*)[notification.userInfo objectForKey:NSWorkspaceApplicationKey];
+#endif
 
-    #if DEBUG
-    NSLog(@"applicationActivated => application: %d-%@", currentApplication.processIdentifier,currentApplication.bundleIdentifier);
-    NSLog(@"applicationActivated => current layouot: %@", TISCopyCurrentKeyboardInputSource());
-    #endif
-    
+    // Take a running application metadata
+    NSRunningApplication *currentApplication = (NSRunningApplication *) notification.userInfo[NSWorkspaceApplicationKey];
+
+#if DEBUG
+    NSLog(@"applicationActivated => application: %d-%@", currentApplication.processIdentifier, currentApplication.bundleIdentifier);
+    //NSLog(@"applicationActivated => current layout: %@", TISCopyCurrentKeyboardInputSource());
+#endif
+
     // Get the current source
     NSString *currentName = [_sourceManager getCurrentInputSourceName];
-    
+
     // Some previous window - we need to retain current layout
-    if (_applicationIdentifier != nil)
-    {
-        #if DEBUG
-        NSLog(@"applicationActivated => previous found %@", _applicationIdentifier);
-        #endif
-    
+    if (_applicationIdentifier != nil) {
+#if DEBUG
+        NSLog(@"applicationActivated => previous application %@", _applicationIdentifier);
+#endif
+
         // Save it
         [_retainedLayouts setValue:currentName forKey:_applicationIdentifier];
-    
-        #if DEBUG
+
+#if DEBUG
         NSLog(@"applicationActivated => retaining previous layout %@ for %@", currentName, _applicationIdentifier);
-        #endif
+#endif
     }
 
     // Let's take a process id as a key for the dictionary
-    _applicationIdentifier = [NSString stringWithFormat:@"%d-%@", currentApplication.processIdentifier,currentApplication.bundleIdentifier];
-    
+    _applicationIdentifier = [NSString stringWithFormat:@"%d-%@", currentApplication.processIdentifier, currentApplication.bundleIdentifier];
+
     // Looking for process id in the dictionary
-    NSString *applicationSourceName = [_retainedLayouts objectForKey:_applicationIdentifier];
-    
+    NSString *applicationSourceName = _retainedLayouts[_applicationIdentifier];
+
     // Change the input source layout if found
-    if (applicationSourceName == nil)
-    {
+    if (applicationSourceName == nil) {
         // Set the default input source
         applicationSourceName = DEFAULT_INPUT_SOURCE;
-    
-        #if DEBUG
+
+#if DEBUG
         NSLog(@"applicationActivated => no retained. Using default %@", applicationSourceName);
-        #endif
+#endif
     }
-    
-    #if DEBUG
+
+#if DEBUG
     NSLog(@"applicationActivated => switching layout to: %@", applicationSourceName);
-    #endif
-    
+#endif
+
     // Switching to new layout whether retained or default
     [_sourceManager selectInputSourceByName:applicationSourceName];
-    
-    #if DEBUG
+
+#if DEBUG
     NSLog(@"applicationActivated => retained Layouts: %@", _retainedLayouts);
-    #endif
+#endif
 }
-
-void OnCurrentTextInputSourceChange(CFNotificationCenterRef aCenter, void* aObserver, CFStringRef aName, const void* aObject, CFDictionaryRef aUserInfo)
-{
-    [(__bridge id)aObserver inputSourceChanged];
-}
-
--(void)inputSourceChanged
-{
-    // Get the current input source
-    //TISInputSourceRef currentSource = TISCopyCurrentKeyboardInputSource();
-    
-    #if DEBUG
-    NSLog(@"inputSourceChanged => current input source: %@", [_sourceManager getCurrentInputSourceName]);
-    #endif
-    
-    /*NSString *currentName = [_sourceManager getInputSourceName:currentSource];
-    
-    // Assign the current layout to the current active process
-    if (_applicationIdentifier != nil)
-    {
-        // Save it in the dictionary
-        [_retainedLayouts setValue:currentName forKey:_applicationIdentifier];
-   
-        #if DEBUG
-        NSLog(@"inputSourceChanged => retaining %@ for %@", currentName, _applicationIdentifier);
-        #endif
-    }
-    
-    #if DEBUG
-    NSLog(@"applicationActivated => retained Layouts: %@", _retainedLayouts);
-    #endif
-    */
-}
-
-/*
--(void)loadMenu
-{
-    NSUInteger n = [[menu itemArray] count];
-    for (int i=0;i<n-2;i++) {
-        [menu removeItemAtIndex:0];
-    }
-    
-    // Reverse order just for have U.S. at the top. No need extra logic of sorting, etc.
-    for(NSInteger i = _inputLayoutList.count-1; i >= 0 ; i--)
-    {
-        TISInputSourceRef sourceRef = (__bridge TISInputSourceRef)([_inputLayoutList objectAtIndex:i]);
-    
-        NSString *sourceName = [self getInputSourceName:sourceRef];
-        #if DEBUG
-        NSLog(@"loadMenu => source name: %@", sourceName);
-        #endif
-    
-        NSImage *image = [self getInputSourceImage:sourceRef];
-            
-        NSMenuItem *menuItem = [menu insertItemWithTitle:sourceName action:false keyEquivalent:@"" atIndex:0];
-        [menuItem setTarget:self];
-        [menuItem setImage:image];
-        [menuItem setEnabled:false];
-        [menuItem setAction:@selector(menuItemAction:)];
-        [menuItem setRepresentedObject:(__bridge id)(sourceRef)];
-    }
-}
-
--(void)menuItemAction:(NSMenuItem *)sender
-{
-    //NSLog(@"sender: %@", sender);
-    TISInputSourceRef requiredSource = (__bridge TISInputSourceRef)(sender.representedObject);
-    TISInputSourceRef currentSource = TISCopyCurrentKeyboardInputSource();
-    
-    //Let's compare based on the name
-    NSString *requiredName = [self getInputSourceName:requiredSource];
-    NSString *currentName = [self getInputSourceName:currentSource];
-    
-    #if DEBUG
-    NSLog(@"menuItemAction => currentName: %@", currentName);
-    NSLog(@"menuItemAction => requiredName: %@", requiredName);
-    #endif
-    
-    // If the current <> required - then need to activate
-    if (![currentName isEqualToString:requiredName])
-    {
-        #if DEBUG
-        NSLog(@"menuItemAction => Switching layout to: %@", requiredName);
-        #endif
-    
-        TISSelectInputSource(requiredSource);
-    }
-}
-
-*/
-
 
 @end
--- KeyboardLayoutRetainer/SourceManager.m
+++ KeyboardLayoutRetainer/SourceManager.m
@@ -12,65 +12,61 @@ NSString * const DEFAULT_INPUT_SOURCE = @"U.S.";
 
 @implementation SourceManager
 
--(TISInputSourceRef)getInputSourceByName:(NSString*)name
-{
-    NSDictionary *filter = [NSDictionary dictionaryWithObject:(NSString *)kTISCategoryKeyboardInputSource
-                                                       forKey:(NSString *)kTISPropertyInputSourceCategory];
-    
-    CFArrayRef cfInputList = (TISCreateInputSourceList((__bridge CFDictionaryRef)filter,FALSE));
-    
-    TISInputSourceRef result = nil;
-    
-    // The default is U.S.
-    for (int i = 0; i < CFArrayGetCount(cfInputList); ++i)
-    {
-        TISInputSourceRef nextInputSource = (TISInputSourceRef)CFArrayGetValueAtIndex(cfInputList, i);
-    
-        NSString *inputSourceName = CFBridgingRelease(TISGetInputSourceProperty(nextInputSource, kTISPropertyLocalizedName));
+//-(TISInputSourceRef)getInputSourceByName:(NSString*)name
+//{
+//    NSDictionary *filter = @{(NSString *) kTISPropertyInputSourceCategory: (NSString *) kTISCategoryKeyboardInputSource};
+//
+//    CFArrayRef cfInputList = (TISCreateInputSourceList((__bridge CFDictionaryRef)filter,FALSE));
+//
+//    TISInputSourceRef result = nil;
+//
+//    // The default is U.S.
+//    for (int i = 0; i < CFArrayGetCount(cfInputList); ++i)
+//    {
+//        TISInputSourceRef nextInputSource = (TISInputSourceRef)CFArrayGetValueAtIndex(cfInputList, i);
+//
+//        NSString *inputSourceName = CFBridgingRelease(TISGetInputSourceProperty(nextInputSource, kTISPropertyLocalizedName));
+//
+//        // If the default equal to the current - save it to result and exit
+//        if ([inputSourceName isEqualToString:name])
+//        {
+//            result = nextInputSource;
+//            break;
+//        }
+//    }
+//
+//    return result;
+//}
 
-        // If the default equal to the current - save it to result and exit
-        if ([inputSourceName isEqualToString:name])
-        {
-            result = nextInputSource;
-            break;
-        }
-    }
-
-    return result;
-}
-
--(NSString*)getInputSourceName:(TISInputSourceRef)source
-{
-    NSString *name = (__bridge NSString *)(TISGetInputSourceProperty(source, kTISPropertyLocalizedName));    
-    return name;
-}
-
--(NSImage*)getInputSourceImage:(TISInputSourceRef)source
-{
-    IconRef icon = TISGetInputSourceProperty(source, kTISPropertyIconRef);
-    NSImage *image = [[NSImage alloc] initWithIconRef:icon];
-    return image;
-}
+//-(NSString*)getInputSourceName:(TISInputSourceRef)source
+//{
+//    NSString *name = (__bridge NSString *)(TISGetInputSourceProperty(source, kTISPropertyLocalizedName));
+//    return name;
+//}
+//
+//-(NSImage*)getInputSourceImage:(TISInputSourceRef)source
+//{
+//    IconRef icon = TISGetInputSourceProperty(source, kTISPropertyIconRef);
+//    NSImage *image = [[NSImage alloc] initWithIconRef:icon];
+//    return image;
+//}
 
 -(void)selectInputSourceByName:(NSString*)name
 {
-    NSDictionary *filter = [NSDictionary dictionaryWithObject:(NSString *)kTISCategoryKeyboardInputSource
-                                                       forKey:(NSString *)kTISPropertyInputSourceCategory];
-    
+    NSDictionary *filter = @{(NSString *) kTISPropertyInputSourceCategory: (NSString *) kTISCategoryKeyboardInputSource};
     CFArrayRef cfInputList = (TISCreateInputSourceList((__bridge CFDictionaryRef)filter,FALSE));
     
     // The default is U.S.
     for (int i = 0; i < CFArrayGetCount(cfInputList); ++i)
     {
         TISInputSourceRef inputSource = (TISInputSourceRef)CFArrayGetValueAtIndex(cfInputList, i);
-    
         NSString *inputSourceName = CFBridgingRelease(TISGetInputSourceProperty(inputSource, kTISPropertyLocalizedName));
 
         // If the default equal to the current - save it to result and exit
         if ([inputSourceName isEqualToString:name])
         {
             TISSelectInputSource(inputSource);
-            [NSThread sleepForTimeInterval:0.1];
+            //[NSThread sleepForTimeInterval:0.1];
             break;
         }
     }
